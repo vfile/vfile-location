bd15b85a0be16cb6ba3b311febbaa1fb0c42d3d6
Titus Wormer <tituswormer@gmail.com>
2023-02-07 14:49:16 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {VFile} from 'vfile'
 import {location} from './index.js'
 
-test('location()', function (t) {
+test('location', function () {
   let place = location('')
 
-  t.equals(
+  assert.equal(
     typeof place.toOffset,
     'function',
     'should expose `toOffset` for `doc`'
   )
 
-  t.equals(
+  assert.equal(
     typeof place.toOffset,
     'function',
     'should expose `toPoint` for `doc`'
@@ -19,176 +20,168 @@ test('location()', function (t) {
 
   place = location(new VFile())
 
-  t.equals(
+  assert.equal(
     typeof place.toOffset,
     'function',
     'should expose `toOffset` for `file`'
   )
 
-  t.equals(
+  assert.equal(
     typeof place.toOffset,
     'function',
     'should expose `toPoint` for `file`'
   )
+})
 
-  t.test('place.toOffset(point)', function (t) {
-    const place = location('foo\nbar\nbaz')
+test('toOffset(point)', function () {
+  const place = location('foo\nbar\nbaz')
 
-    t.equals(
-      place.toOffset({line: undefined, column: undefined}),
-      -1,
-      'should return `-1` for invalid input'
-    )
+  assert.equal(
+    place.toOffset({line: undefined, column: undefined}),
+    -1,
+    'should return `-1` for invalid input'
+  )
 
-    t.equals(
-      place.toOffset({line: 4, column: 2}),
-      -1,
-      'should return `-1` for out of bounds input'
-    )
+  assert.equal(
+    place.toOffset({line: 4, column: 2}),
+    -1,
+    'should return `-1` for out of bounds input'
+  )
 
-    t.equals(
-      place.toOffset({line: 2, column: 2}),
-      5,
-      'should return an offset (#1)'
-    )
+  assert.equal(
+    place.toOffset({line: 2, column: 2}),
+    5,
+    'should return an offset (#1)'
+  )
 
-    t.equals(
-      place.toOffset({line: 1, column: 1}),
-      0,
-      'should return an offset (#2)'
-    )
+  assert.equal(
+    place.toOffset({line: 1, column: 1}),
+    0,
+    'should return an offset (#2)'
+  )
 
-    t.equals(
-      place.toOffset({line: 3, column: 4}),
-      11,
-      'should return an offset (#3)'
-    )
+  assert.equal(
+    place.toOffset({line: 3, column: 4}),
+    11,
+    'should return an offset (#3)'
+  )
+})
 
-    t.end()
-  })
+test('toPoint(offset)', function () {
+  const place = location('foo\nbar\nbaz')
 
-  t.test('place.toPoint(offset)', function (t) {
-    const place = location('foo\nbar\nbaz')
+  assert.deepEqual(
+    place.toPoint(-1),
+    {line: undefined, column: undefined, offset: undefined},
+    'should return an empty object for invalid input'
+  )
 
-    t.deepEquals(
-      place.toPoint(-1),
+  assert.deepEqual(
+    place.toPoint(12),
+    {line: undefined, column: undefined, offset: undefined},
+    'should return an empty object for out of bounds input'
+  )
+
+  assert.deepEqual(
+    place.toPoint(0),
+    {line: 1, column: 1, offset: 0},
+    'should return a point (#1)'
+  )
+
+  assert.deepEqual(
+    place.toPoint(11),
+    {line: 3, column: 4, offset: 11},
+    'should return a point (#2)'
+  )
+})
+
+test('other tests', function () {
+  let place = location('foo')
+
+  assert.deepEqual(
+    [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
+    [
+      {line: 1, column: 4, offset: 3},
       {line: undefined, column: undefined, offset: undefined},
-      'should return an empty object for invalid input'
-    )
+      {line: undefined, column: undefined, offset: undefined}
+    ],
+    'should return points for offsets around an EOF w/o EOLs'
+  )
 
-    t.deepEquals(
-      place.toPoint(12),
-      {line: undefined, column: undefined, offset: undefined},
-      'should return an empty object for out of bounds input'
-    )
+  assert.deepEqual(
+    [
+      place.toOffset({line: 1, column: 4}),
+      place.toOffset({line: 2, column: 1}),
+      place.toOffset({line: 2, column: 2})
+    ],
+    [3, -1, -1],
+    'should return offsets for points around an EOF w/o EOLs'
+  )
 
-    t.deepEquals(
-      place.toPoint(0),
-      {line: 1, column: 1, offset: 0},
-      'should return a point (#1)'
-    )
+  place = location('foo\n')
 
-    t.deepEquals(
-      place.toPoint(11),
-      {line: 3, column: 4, offset: 11},
-      'should return a point (#2)'
-    )
+  assert.deepEqual(
+    [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
+    [
+      {line: 1, column: 4, offset: 3},
+      {line: 2, column: 1, offset: 4},
+      {line: undefined, column: undefined, offset: undefined}
+    ],
+    'should return points for offsets around an EOF EOL'
+  )
 
-    t.end()
-  })
+  assert.deepEqual(
+    [
+      place.toOffset({line: 1, column: 4}),
+      place.toOffset({line: 2, column: 1}),
+      place.toOffset({line: 2, column: 2})
+    ],
+    [3, 4, -1],
+    'should return offsets for points around an EOF EOL'
+  )
 
-  t.test('other tests', function (t) {
-    let place = location('foo')
+  place = location('foo\rbar')
 
-    t.deepEquals(
-      [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
-      [
-        {line: 1, column: 4, offset: 3},
-        {line: undefined, column: undefined, offset: undefined},
-        {line: undefined, column: undefined, offset: undefined}
-      ],
-      'should return points for offsets around an EOF w/o EOLs'
-    )
+  assert.deepEqual(
+    [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
+    [
+      {line: 1, column: 4, offset: 3},
+      {line: 2, column: 1, offset: 4},
+      {line: 2, column: 2, offset: 5}
+    ],
+    'should return points for offsets around carriage returns'
+  )
 
-    t.deepEquals(
-      [
-        place.toOffset({line: 1, column: 4}),
-        place.toOffset({line: 2, column: 1}),
-        place.toOffset({line: 2, column: 2})
-      ],
-      [3, -1, -1],
-      'should return offsets for points around an EOF w/o EOLs'
-    )
+  assert.deepEqual(
+    [
+      place.toOffset({line: 1, column: 4}),
+      place.toOffset({line: 2, column: 1}),
+      place.toOffset({line: 2, column: 2})
+    ],
+    [3, 4, 5],
+    'should return offsets for points around carriage returns'
+  )
 
-    place = location('foo\n')
+  place = location('foo\r\nbar')
 
-    t.deepEquals(
-      [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
-      [
-        {line: 1, column: 4, offset: 3},
-        {line: 2, column: 1, offset: 4},
-        {line: undefined, column: undefined, offset: undefined}
-      ],
-      'should return points for offsets around an EOF EOL'
-    )
+  assert.deepEqual(
+    [place.toPoint(3), place.toPoint(4), place.toPoint(5), place.toPoint(6)],
+    [
+      {line: 1, column: 4, offset: 3},
+      {line: 1, column: 5, offset: 4},
+      {line: 2, column: 1, offset: 5},
+      {line: 2, column: 2, offset: 6}
+    ],
+    'should return points for offsets around carriage returns + line feeds'
+  )
 
-    t.deepEquals(
-      [
-        place.toOffset({line: 1, column: 4}),
-        place.toOffset({line: 2, column: 1}),
-        place.toOffset({line: 2, column: 2})
-      ],
-      [3, 4, -1],
-      'should return offsets for points around an EOF EOL'
-    )
-
-    place = location('foo\rbar')
-
-    t.deepEquals(
-      [place.toPoint(3), place.toPoint(4), place.toPoint(5)],
-      [
-        {line: 1, column: 4, offset: 3},
-        {line: 2, column: 1, offset: 4},
-        {line: 2, column: 2, offset: 5}
-      ],
-      'should return points for offsets around carriage returns'
-    )
-
-    t.deepEquals(
-      [
-        place.toOffset({line: 1, column: 4}),
-        place.toOffset({line: 2, column: 1}),
-        place.toOffset({line: 2, column: 2})
-      ],
-      [3, 4, 5],
-      'should return offsets for points around carriage returns'
-    )
-
-    place = location('foo\r\nbar')
-
-    t.deepEquals(
-      [place.toPoint(3), place.toPoint(4), place.toPoint(5), place.toPoint(6)],
-      [
-        {line: 1, column: 4, offset: 3},
-        {line: 1, column: 5, offset: 4},
-        {line: 2, column: 1, offset: 5},
-        {line: 2, column: 2, offset: 6}
-      ],
-      'should return points for offsets around carriage returns + line feeds'
-    )
-
-    t.deepEquals(
-      [
-        place.toOffset({line: 1, column: 4}),
-        place.toOffset({line: 2, column: 1}),
-        place.toOffset({line: 2, column: 2})
-      ],
-      [3, 5, 6],
-      'should return offsets for points around carriage returns + line feeds'
-    )
-
-    t.end()
-  })
-
-  t.end()
+  assert.deepEqual(
+    [
+      place.toOffset({line: 1, column: 4}),
+      place.toOffset({line: 2, column: 1}),
+      place.toOffset({line: 2, column: 2})
+    ],
+    [3, 5, 6],
+    'should return offsets for points around carriage returns + line feeds'
+  )
 })
